9625ba391c54c5d8695b8e1e71c41c286333bc69
Yannick Robert <yrobert@apption.com>
2020-04-01 10:08:23 -0400

    updated license comments

--- ElementalDocs/Program.cs
+++ ElementalDocs/Program.cs
@@ -22,6 +22,7 @@ namespace ElementalDocs
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseStaticWebAssets();
                     webBuilder.UseStartup<Startup>();
                 });
     }
