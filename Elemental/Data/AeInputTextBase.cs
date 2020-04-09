00b087a08f8247b22bd7295c3b1578e9a3a4dd4b
Yannick Robert <yrobert@apption.com>
2020-04-09 12:27:14 -0400

    wip on model form

---
+++ Elemental/Data/AeInputTextBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Elemental.Data
+{
+    public class AeInputTextBase : InputBase<string>
+    {
+        [Parameter]
+        public string Id { get; set; }
+        [Parameter]
+        public string Label { get; set; }
+        [Parameter]
+        public Expression<Func<string>> ValidationFor { get; set; }
+
+        protected override bool TryParseValueFromString(string value, out string result, out string validationErrorMessage)
+        {
+            result = value;
+            validationErrorMessage = null;
+            return true;
+        }
+    }
+}
