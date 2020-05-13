36c60890819731d43c27c110a1745de6738ad203
Yannick Robert <yrobert@apption.com>
2020-05-12 20:45:20 -0400

    added descriptive labels

--- ElementalDocs/Data/Starship.cs
+++ ElementalDocs/Data/Starship.cs
@@ -8,7 +8,6 @@ namespace ElementalDocs.Data
     {
         [Required]
         [StringLength(16, ErrorMessage = "Identifier too long (16 character limit).")]
-        [AeLabelAttribute("Idstaaay")]
         public string Identifier { get; set; }
 
         public string Description { get; set; }
@@ -17,13 +16,16 @@ namespace ElementalDocs.Data
         public string Classification { get; set; }
 
         [Range(1, 100000, ErrorMessage = "Accommodation invalid (1-100000).")]
+        [AeLabelAttribute("Maximum Accomodation")]
         public int MaximumAccommodation { get; set; }
 
         [Required]
         [Range(typeof(bool), "true", "true", ErrorMessage = "This form disallows unapproved ships.")]
+        [AeLabelAttribute("Validated Design")]
         public bool IsValidatedDesign { get; set; }
 
         [Required]
+        [AeLabelAttribute("Production Date")]
         public DateTime ProductionDate { get; set; }
     }
 }
