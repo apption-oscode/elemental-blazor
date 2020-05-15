d3a11111a1a6ad06c6bb261f1d40f1065fa87568
Yannick Robert <yrobert@apption.com>
2020-05-15 00:40:17 -0400

    added initial values to dropdown

---
+++ Elemental/Code/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elemental.Code
+{
+    public static class Extensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null)
+        {
+            return new HashSet<T>(source, comparer);
+        }
+    }
+}
