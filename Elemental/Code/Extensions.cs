84c8b75d2e2b25babdc9f6890e5f66050dbf1403
dzhao <dzhao@P51-COOP>
2020-06-05 11:53:59 -0400

    Updated table with pagination

--- Elemental/Code/Extensions.cs
+++ Elemental/Code/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Elemental.Code
@@ -9,6 +10,24 @@ namespace Elemental.Code
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null)
         {
             return new HashSet<T>(source, comparer);
+        }
+
+        /// <summary>
+        /// Used for pagination
+        /// </summary>        
+        //used by LINQ to SQL
+        public static IQueryable<TSource> Page<TSource>(this IQueryable<TSource> source, int pageIndex, int pageSize)
+        {
+            return source.Skip(pageIndex * pageSize).Take(pageSize);
+        }
+
+        /// <summary>
+        /// Used for pagination
+        /// </summary>       
+        //used by LINQ
+        public static IEnumerable<TSource> Page<TSource>(this IEnumerable<TSource> source, int pageIndex, int pageSize)
+        {
+            return source.Skip(pageIndex * pageSize).Take(pageSize);
         }
     }
 }
---
+++ Elemental/Code/PageStatus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elemental.Code
+{
+    public class PageStatus
+    {
+        /// <summary>
+        /// Start from 1
+        /// </summary>
+        public int CurrentPage { get; set; }
+        /// <summary>
+        /// Number of pages
+        /// </summary>
+        public int PageCount { get; set; }
+        /// <summary>
+        /// Number of items inside a page
+        /// </summary>
+        public int PageSize { get; set; }
+
+        public PageStatus (int pageCount, int pagesize)
+        {
+            CurrentPage = 1;
+            PageCount = pageCount;
+            PageSize = pagesize;
+        }
+    }
+}
