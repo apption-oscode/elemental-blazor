7b4a74b308a1f87228405823da00297897e97c25
Yannick Robert <yrobert@apption.com>
2020-08-15 12:51:35 -0400

    added file parser for source demos

---
+++ ElementalDocs/Tools/ParsedFile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElementalDocs
+{
+    public class ParsedFile
+    {
+        public string Pathname { get; private set; }
+        public string Title { get; private set; }
+        public object Description { get; private set; }
+        public List<string> Html { get; private set; }
+        public List<string> Code { get; private set; }
+
+        public ParsedFile (string path)
+        {
+            Pathname = path;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var lines = ReadLines(Pathname);
+            Title = ParseTitle(lines);
+            Description = ParseDescription(lines);
+            Html = ParseHtml(lines);
+            Code = ParseCode(lines);
+        }
+
+
+        public static string[] ReadLines(string pathname)
+        {
+            return File.ReadAllLines(pathname);
+        }
+
+        public static string ParseTitle(string[] lines)
+        {
+            return lines
+                .FirstOrDefault(
+                    l => l.Contains("Title"))
+                .Split(":")
+                .Skip(1)
+                .FirstOrDefault()
+                .Trim();
+        }
+
+        public static string ParseDescription(string[] lines)
+        {
+            return lines
+                .FirstOrDefault(
+                    l => l.Contains("Description"))
+                .Split(":")
+                .Skip(1)
+                .FirstOrDefault()
+                .Trim();
+        }
+
+        public static List<string> ParseHtml(string[] lines)
+        {
+            return lines
+                .SkipWhile(l => !l.Equals("*@"))
+                .Skip(1)
+                .TakeWhile(l => !l.Equals("@code {"))
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .ToList();
+        }
+
+        public static List<string> ParseCode(string[] lines)
+        {
+            return lines
+                .SkipWhile(l => !l.Equals("@code {"))
+                .ToList();
+        }
+    }
+}
---
+++ ElementalTests/SampleParsingTests.cs
@@ -0,0 +1,79 @@
+﻿using ElementalDocs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ElementalTests
+{
+    public class SampleParsingTests
+    {
+        private const string _testFilePath = "./Resources/ParsingTestComponent.razor";
+        private const int _testFileLineCount = 34;
+        private const string _testTitle = "Test Component";
+        private const string _testDescription = "Lorem ipsum dolor sit amet.";
+        private const int _testHtmlLength = 6;
+        private const int _testCodeLength = 14;
+
+        [Fact]
+        public void CanReadFile()
+        {
+            var lines = ParsedFile.ReadLines(_testFilePath);
+            
+            Assert.NotNull(lines);
+            Assert.Equal(_testFileLineCount, lines.Length);
+        }
+
+        [Fact]
+        public void CanFindTitle()
+        {
+            var lines = ParsedFile.ReadLines(_testFilePath);
+            var title = ParsedFile.ParseTitle(lines);
+            
+            Assert.Equal(_testTitle, title);
+        }
+
+        [Fact]
+        public void CanFindDescription()
+        {
+            var lines = ParsedFile.ReadLines(_testFilePath);
+            var description = ParsedFile.ParseDescription(lines);
+
+            Assert.Equal(_testDescription, description);
+        }
+
+        [Fact]
+        public void CanFindHtml()
+        {
+            var lines = ParsedFile.ReadLines(_testFilePath);
+            var html = ParsedFile.ParseHtml(lines);
+
+            Assert.Equal(_testHtmlLength, html.Count);
+            Assert.Contains(@"<div class=""test-1"">one</div>", html.First());
+            Assert.Contains(@"</div>", html.Last());
+        }
+
+        [Fact]
+        public void CanFindCode()
+        {
+            var lines = ParsedFile.ReadLines(_testFilePath);
+            var code = ParsedFile.ParseCode(lines);
+
+            Assert.Equal(_testCodeLength, code.Count);
+            Assert.Equal("@code {", code.First());
+            Assert.Equal("}", code.Last());
+        }
+
+        [Fact]
+        public void CanSetupParsedFile()
+        {
+            var file = new ParsedFile(_testFilePath);
+
+            Assert.Equal(_testTitle, file.Title);
+            Assert.Equal(_testDescription, file.Description);
+            Assert.Equal(_testHtmlLength, file.Html.Count);
+            Assert.Equal(_testCodeLength, file.Code.Count);
+        }
+    }
+}
