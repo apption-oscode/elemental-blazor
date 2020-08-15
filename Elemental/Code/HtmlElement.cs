43c100fcb0582f66279c620877dca10185aa3261
Yannick Robert <yrobert@apption.com>
2020-08-15 12:51:48 -0400

    wip update on documentation

---
+++ Elemental/Code/HtmlElement.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elemental.Code
+{
+    public class HtmlElement : ComponentBase
+    {
+        [Parameter]
+        public string Classname { get; set; }
+        [Parameter]
+        public string Style { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public string Id { get; set; }
+    }
+}
