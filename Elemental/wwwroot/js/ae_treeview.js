bd8b98287b25cfbfeabcd0d935a9114adb37d576
Nick Salt <nsalt@apption.com>
2021-03-02 14:44:47 -0500

    continued work

---
+++ Elemental/wwwroot/js/ae_treeview.js
@@ -0,0 +1,8 @@
+﻿window.aetreeview = {
+    setCheckboxIndeterminate: (id) => {
+        document.getElementById(id).indeterminate = true;
+    },
+    setCheckboxDeterminate: (id) => {
+        document.getElementById(id).indeterminate = false;
+    },
+}
