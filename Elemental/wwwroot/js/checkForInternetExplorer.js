7d97da56d6d3b822afd0914d6c8cb4658d212c37
nicksalt <nicksalt24@gmail.com>
2021-04-14 10:17:05 -0400

    ie check works in blazor server side

---
+++ Elemental/wwwroot/js/checkForInternetExplorer.js
@@ -0,0 +1,7 @@
+﻿//simple script that checks if current browser is internet explorer
+window.isInternetExplorer = () => {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    return (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))
+};
