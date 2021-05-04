cdd1a6f287d42bc4783a76b46b7bcbd67955a99d
Shashaank Srivastava <sSrivastava@apption.com>
2021-05-04 15:24:22 -0400

    Fragment routing component created

---
+++ Elemental/wwwroot/js/anchor.js
@@ -0,0 +1,10 @@
+﻿function BlazorScrollToId(id) {
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+    element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    });
+    }
+}
