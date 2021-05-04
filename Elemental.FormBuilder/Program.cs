7da74be2b56a4a8c4ee8fa54c17522e84efd79b6
Erik Putrycz <erik.putrycz@nrcan-rncan.gc.ca>
2021-05-04 16:43:49 -0400

    - removed migrations - added db creation in startup - added href base

--- Elemental.FormBuilder/Program.cs
+++ Elemental.FormBuilder/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Elemental.FormBuilder.Code;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -15,7 +18,17 @@ namespace Elemental.FormBuilder
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var formDb = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                formDb.Database.EnsureCreated();
+                formDb.Database.Migrate();
+
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
