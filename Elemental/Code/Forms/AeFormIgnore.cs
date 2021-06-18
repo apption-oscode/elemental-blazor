390e7dcd6c27b0f9800d3e146a7acdb2f44bee11
Yannick Robert <yrobert@apption.com>
2021-06-18 09:05:03 -0400

    moved forms code into code folder and updated namespaces

--- Elemental/Code/Forms/AeFormIgnore.cs
+++ Elemental/Code/Forms/AeFormIgnore.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Elemental.Components.Forms
+namespace Elemental.Code.Forms
 {
     public class AeFormIgnoreAttribute : Attribute
     {
--- Elemental/Code/Forms/ModelFormContext.cs
+++ Elemental/Code/Forms/ModelFormContext.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Elemental.Components.Forms
+namespace Elemental.Code.Forms
 {
 
     public class ModelFormArgsInternal
