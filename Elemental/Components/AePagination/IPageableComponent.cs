47b569ffa4c6f9fd7d058c8742fcd3d5ed956fa1
Yannick Robert <yrobert@apption.com>
2021-07-13 15:49:41 -0400

    fixed a paging bug

--- Elemental/Components/AePagination/IPageableComponent.cs
+++ Elemental/Components/AePagination/IPageableComponent.cs
@@ -7,6 +7,6 @@ namespace Elemental.Components
     public int Page { get; set; }
     public int PageSize { get; set; }
     public int Total { get; set; }
-    public int PageCount => Total / Math.Max(1, PageSize);
+    public int PageCount => ((int)Math.Ceiling(Total / (decimal) Math.Max(1, PageSize)));
   }
 }
