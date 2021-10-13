2efe646c5a082c73fbf9e84b99cfeeb58f6c448a
Minh <minhja2014@gmail.com>
2021-10-13 11:06:16 -0400

    Fixed PaginatedTable sorting bug Sorting the paginated table sets the page number to 1

--- Elemental/Components/AePagination/IPageableComponent.cs
+++ Elemental/Components/AePagination/IPageableComponent.cs
@@ -7,6 +7,7 @@ namespace Elemental.Components
         public int Page { get; set; }
         public int PageSize { get; set; }
         public int Total { get; set; }
+        public AePagination Paginator { get; set; }
         public int PageCount => ((int)Math.Ceiling(Total / (decimal)Math.Max(1, PageSize)));
     }
 }
