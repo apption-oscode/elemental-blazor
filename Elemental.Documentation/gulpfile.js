ff74adbe10264dbd7dc2147e7aac0ebe6fb582f3
Erik <erik@apption.com>
2022-02-14 15:26:12 -0500

    added gulp build to copy shepherd from npm to js folder

---
+++ Elemental.Documentation/gulpfile.js
@@ -0,0 +1,14 @@
+﻿var gulp = require('gulp'),
+    cleanDest = require('gulp-clean-dest'),
+    flatten = require('gulp-flatten')
+    cache = require('gulp-cached'); //If cached version identical to current file then it doesn't pass it downstream so this file won't be copied 
+
+function copyFiles() {
+    return gulp.src('node_modules/shepherd.js/**/shepherd.min.js')
+        .pipe(cache('node_modules'))
+        .pipe(flatten())
+        //.pipe(cleanDest('.'))
+        .pipe(gulp.dest('wwwroot/js'));
+}
+
+gulp.task('default', copyFiles);
