f5068fbb8ba2216155f7333363ca151a0be5a5a5
Erik <erik@apption.com>
2022-02-15 15:22:23 -0500

    regulped stuff

--- Elemental.Documentation/gulpfile.js
+++ Elemental.Documentation/gulpfile.js
@@ -4,11 +4,11 @@
     cache = require('gulp-cached'); //If cached version identical to current file then it doesn't pass it downstream so this file won't be copied 
 
 function copyFiles() {
-    return gulp.src('node_modules/shepherd.js/**/shepherd.min.js')
+    return gulp.src('node_modules/shepherd.js/**/shepherd.min.js', 'node_modules/highlight.js/**/highlight.min.js')
         .pipe(cache('node_modules'))
         .pipe(flatten())
         //.pipe(cleanDest('.'))
-        .pipe(gulp.dest('wwwroot/js'));
+        .pipe(gulp.dest('wwwroot/js/third-party'));
 }
 
 gulp.task('default', copyFiles);
