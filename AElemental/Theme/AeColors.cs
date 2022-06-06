392272785e5be8b58e08213dc0a9b7f7a694d9c9
Yan <yrobert@apption.com>
2022-06-06 11:45:20 -0400

    added tailwindcss colors to AeColors.cs

---
+++ AElemental/Theme/AeColors.cs
@@ -0,0 +1,252 @@
+namespace AElemental.Theme;
+
+public class AeColors
+{
+    public readonly string Inherit = "inherit";
+    public readonly string Current = "currentColor";
+    public readonly string Transparent = "transparent";
+    public readonly string Black = "#000";
+    public readonly string White = "#fff";
+
+    public readonly string Slate50 = "#f8fafc";
+    public readonly string Slate100 = "#f1f5f9";
+    public readonly string Slate200 = "#e2e8f0";
+    public readonly string Slate300 = "#cbd5e1";
+    public readonly string Slate400 = "#94a3b8";
+    public readonly string Slate500 = "#64748b";
+    public readonly string Slate600 = "#475569";
+    public readonly string Slate700 = "#334155";
+    public readonly string Slate800 = "#1e293b";
+    public readonly string Slate900 = "#0f172a";
+
+    public readonly string Gray50 = "#f9fafb";
+    public readonly string Gray100 = "#f3f4f6";
+    public readonly string Gray200 = "#e5e7eb";
+    public readonly string Gray300 = "#d1d5db";
+    public readonly string Gray400 = "#9ca3af";
+    public readonly string Gray500 = "#6b7280";
+    public readonly string Gray600 = "#4b5563";
+    public readonly string Gray700 = "#374151";
+    public readonly string Gray800 = "#1f2937";
+    public readonly string Gray900 = "#111827";
+
+    public readonly string Zinc50 = "#fafafa";
+    public readonly string Zinc100 = "#f4f4f5";
+    public readonly string Zinc200 = "#e4e4e7";
+    public readonly string Zinc300 = "#d4d4d8";
+    public readonly string Zinc400 = "#a1a1aa";
+    public readonly string Zinc500 = "#71717a";
+    public readonly string Zinc600 = "#52525b";
+    public readonly string Zinc700 = "#3f3f46";
+    public readonly string Zinc800 = "#27272a";
+    public readonly string Zinc900 = "#18181b";
+
+    public readonly string Neutral50 = "#fafafa";
+    public readonly string Neutral100 = "#f5f5f5";
+    public readonly string Neutral200 = "#e5e5e5";
+    public readonly string Neutral300 = "#d4d4d4";
+    public readonly string Neutral400 = "#a3a3a3";
+    public readonly string Neutral500 = "#737373";
+    public readonly string Neutral600 = "#525252";
+    public readonly string Neutral700 = "#404040";
+    public readonly string Neutral800 = "#262626";
+    public readonly string Neutral900 = "#171717";
+
+    public readonly string Stone50 = "#fafaf9";
+    public readonly string Stone100 = "#f5f5f4";
+    public readonly string Stone200 = "#e7e5e4";
+    public readonly string Stone300 = "#d6d3d1";
+    public readonly string Stone400 = "#a8a29e";
+    public readonly string Stone500 = "#78716c";
+    public readonly string Stone600 = "#57534e";
+    public readonly string Stone700 = "#44403c";
+    public readonly string Stone800 = "#292524";
+    public readonly string Stone900 = "#1c1917";
+
+    public readonly string Red50 = "#fef2f2";
+    public readonly string Red100 = "#fee2e2";
+    public readonly string Red200 = "#fecaca";
+    public readonly string Red300 = "#fca5a5";
+    public readonly string Red400 = "#f87171";
+    public readonly string Red500 = "#ef4444";
+    public readonly string Red600 = "#dc2626";
+    public readonly string Red700 = "#b91c1c";
+    public readonly string Red800 = "#991b1b";
+    public readonly string Red900 = "#7f1d1d";
+
+    public readonly string Orange50 = "#fff7ed";
+    public readonly string Orange100 = "#ffedd5";
+    public readonly string Orange200 = "#fed7aa";
+    public readonly string Orange300 = "#fdba74";
+    public readonly string Orange400 = "#fb923c";
+    public readonly string Orange500 = "#f97316";
+    public readonly string Orange600 = "#ea580c";
+    public readonly string Orange700 = "#c2410c";
+    public readonly string Orange800 = "#9a3412";
+    public readonly string Orange900 = "#7c2d12";
+
+    public readonly string Amber50 = "#fffbeb";
+    public readonly string Amber100 = "#fef3c7";
+    public readonly string Amber200 = "#fde68a";
+    public readonly string Amber300 = "#fcd34d";
+    public readonly string Amber400 = "#fbbf24";
+    public readonly string Amber500 = "#f59e0b";
+    public readonly string Amber600 = "#d97706";
+    public readonly string Amber700 = "#b45309";
+    public readonly string Amber800 = "#92400e";
+    public readonly string Amber900 = "#78350f";
+
+    public readonly string Yellow50 = "#fefce8";
+    public readonly string Yellow100 = "#fef9c3";
+    public readonly string Yellow200 = "#fef08a";
+    public readonly string Yellow300 = "#fde047";
+    public readonly string Yellow400 = "#facc15";
+    public readonly string Yellow500 = "#eab308";
+    public readonly string Yellow600 = "#ca8a04";
+    public readonly string Yellow700 = "#a16207";
+    public readonly string Yellow800 = "#854d0e";
+    public readonly string Yellow900 = "#713f12";
+
+    public readonly string Lime50 = "#f7fee7";
+    public readonly string Lime100 = "#ecfccb";
+    public readonly string Lime200 = "#d9f99d";
+    public readonly string Lime300 = "#bef264";
+    public readonly string Lime400 = "#a3e635";
+    public readonly string Lime500 = "#84cc16";
+    public readonly string Lime600 = "#65a30d";
+    public readonly string Lime700 = "#4d7c0f";
+    public readonly string Lime800 = "#3f6212";
+    public readonly string Lime900 = "#365314";
+
+    public readonly string Green50 = "#f0fdf4";
+    public readonly string Green100 = "#dcfce7";
+    public readonly string Green200 = "#bbf7d0";
+    public readonly string Green300 = "#86efac";
+    public readonly string Green400 = "#4ade80";
+    public readonly string Green500 = "#22c55e";
+    public readonly string Green600 = "#16a34a";
+    public readonly string Green700 = "#15803d";
+    public readonly string Green800 = "#166534";
+    public readonly string Green900 = "#14532d";
+
+    public readonly string Emerald50 = "#ecfdf5";
+    public readonly string Emerald100 = "#d1fae5";
+    public readonly string Emerald200 = "#a7f3d0";
+    public readonly string Emerald300 = "#6ee7b7";
+    public readonly string Emerald400 = "#34d399";
+    public readonly string Emerald500 = "#10b981";
+    public readonly string Emerald600 = "#059669";
+    public readonly string Emerald700 = "#047857";
+    public readonly string Emerald800 = "#065f46";
+    public readonly string Emerald900 = "#064e3b";
+
+    public readonly string Teal50 = "#f0fdfa";
+    public readonly string Teal100 = "#ccfbf1";
+    public readonly string Teal200 = "#99f6e4";
+    public readonly string Teal300 = "#5eead4";
+    public readonly string Teal400 = "#2dd4bf";
+    public readonly string Teal500 = "#14b8a6";
+    public readonly string Teal600 = "#0d9488";
+    public readonly string Teal700 = "#0f766e";
+    public readonly string Teal800 = "#115e59";
+    public readonly string Teal900 = "#134e4a";
+
+    public readonly string Cyan50 = "#ecfeff";
+    public readonly string Cyan100 = "#cffafe";
+    public readonly string Cyan200 = "#a5f3fc";
+    public readonly string Cyan300 = "#67e8f9";
+    public readonly string Cyan400 = "#22d3ee";
+    public readonly string Cyan500 = "#06b6d4";
+    public readonly string Cyan600 = "#0891b2";
+    public readonly string Cyan700 = "#0e7490";
+    public readonly string Cyan800 = "#155e75";
+    public readonly string Cyan900 = "#164e63";
+
+    public readonly string Sky50 = "#f0f9ff";
+    public readonly string Sky100 = "#e0f2fe";
+    public readonly string Sky200 = "#bae6fd";
+    public readonly string Sky300 = "#7dd3fc";
+    public readonly string Sky400 = "#38bdf8";
+    public readonly string Sky500 = "#0ea5e9";
+    public readonly string Sky600 = "#0284c7";
+    public readonly string Sky700 = "#0369a1";
+    public readonly string Sky800 = "#075985";
+    public readonly string Sky900 = "#0c4a6e";
+
+    public readonly string Blue50 = "#eff6ff";
+    public readonly string Blue100 = "#dbeafe";
+    public readonly string Blue200 = "#bfdbfe";
+    public readonly string Blue300 = "#93c5fd";
+    public readonly string Blue400 = "#60a5fa";
+    public readonly string Blue500 = "#3b82f6";
+    public readonly string Blue600 = "#2563eb";
+    public readonly string Blue700 = "#1d4ed8";
+    public readonly string Blue800 = "#1e40af";
+    public readonly string Blue900 = "#1e3a8a";
+
+    public readonly string Indigo50 = "#eef2ff";
+    public readonly string Indigo100 = "#e0e7ff";
+    public readonly string Indigo200 = "#c7d2fe";
+    public readonly string Indigo300 = "#a5b4fc";
+    public readonly string Indigo400 = "#818cf8";
+    public readonly string Indigo500 = "#6366f1";
+    public readonly string Indigo600 = "#4f46e5";
+    public readonly string Indigo700 = "#4338ca";
+    public readonly string Indigo800 = "#3730a3";
+    public readonly string Indigo900 = "#312e81";
+
+    public readonly string Violet50 = "#f5f3ff";
+    public readonly string Violet100 = "#ede9fe";
+    public readonly string Violet200 = "#ddd6fe";
+    public readonly string Violet300 = "#c4b5fd";
+    public readonly string Violet400 = "#a78bfa";
+    public readonly string Violet500 = "#8b5cf6";
+    public readonly string Violet600 = "#7c3aed";
+    public readonly string Violet700 = "#6d28d9";
+    public readonly string Violet800 = "#5b21b6";
+    public readonly string Violet900 = "#4c1d95";
+
+    public readonly string Purple50 = "#faf5ff";
+    public readonly string Purple100 = "#f3e8ff";
+    public readonly string Purple200 = "#e9d5ff";
+    public readonly string Purple300 = "#d8b4fe";
+    public readonly string Purple400 = "#c084fc";
+    public readonly string Purple500 = "#a855f7";
+    public readonly string Purple600 = "#9333ea";
+    public readonly string Purple700 = "#7e22ce";
+    public readonly string Purple800 = "#6b21a8";
+    public readonly string Purple900 = "#581c87";
+
+    public readonly string Fuchsia50 = "#fdf4ff";
+    public readonly string Fuchsia100 = "#fae8ff";
+    public readonly string Fuchsia200 = "#f5d0fe";
+    public readonly string Fuchsia300 = "#f0abfc";
+    public readonly string Fuchsia400 = "#e879f9";
+    public readonly string Fuchsia500 = "#d946ef";
+    public readonly string Fuchsia600 = "#c026d3";
+    public readonly string Fuchsia700 = "#a21caf";
+    public readonly string Fuchsia800 = "#86198f";
+    public readonly string Fuchsia900 = "#701a75";
+
+    public readonly string Pink50 = "#fdf2f8";
+    public readonly string Pink100 = "#fce7f3";
+    public readonly string Pink200 = "#fbcfe8";
+    public readonly string Pink300 = "#f9a8d4";
+    public readonly string Pink400 = "#f472b6";
+    public readonly string Pink500 = "#ec4899";
+    public readonly string Pink600 = "#db2777";
+    public readonly string Pink700 = "#be185d";
+    public readonly string Pink800 = "#9d174d";
+    public readonly string Pink900 = "#831843";
+
+    public readonly string Rose50 = "#fff1f2";
+    public readonly string Rose100 = "#ffe4e6";
+    public readonly string Rose200 = "#fecdd3";
+    public readonly string Rose300 = "#fda4af";
+    public readonly string Rose400 = "#fb7185";
+    public readonly string Rose500 = "#f43f5e";
+    public readonly string Rose600 = "#e11d48";
+    public readonly string Rose700 = "#be123c";
+    public readonly string Rose800 = "#9f1239";
+    public readonly string Rose900 = "#881337";
+}
