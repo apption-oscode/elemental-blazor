6ef84e267a2da78b16c3a83c69f0d0d8c4e55eb2
Yan <yrobert@apption.com>
2022-06-13 09:25:34 -0400

    updated code snippet and added icons page

---
+++ AElemental/Components/AeCode/AeCode.razor.cs
@@ -0,0 +1,11 @@
+namespace AElemental.Components;
+
+public partial class AeCode
+{
+    public struct LineTypes
+    {
+        public const string Default = "inline"; 
+        public const string Single = "single";  
+        public const string Multiline = "multi";
+    }
+}
