2423f7257ebe377ea19a5c14ebcabe7a5650809a
Yan <yrobert@apption.com>
2022-06-20 12:18:39 -0400

    added select

--- AElemental/main.ts
+++ AElemental/main.ts
@@ -6,8 +6,9 @@ import 'carbon-web-components/es/components/ui-shell/index.js';
 import 'carbon-web-components/es/components/code-snippet/index.js';
 import 'carbon-web-components/es/components/dropdown/index.js';
 import 'carbon-web-components/es/components/progress-indicator/index.js';
+import 'carbon-web-components/es/components/select/index.js';
 
 import './Components/AeButton/AeButton';
 import './Components/AeCheckbox/AeCheckbox';
-import './Components/AeDropdown/AeDropdown';
+import './Components/AeDropdown/AeDropdown'; 
 
