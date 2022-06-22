10e77c0b2a94f6ddf37b1f10f54dc1ea79b00241
Yan <yrobert@apption.com>
2022-06-22 08:56:59 -0400

    added tooltip directions AB#2862

--- AElemental/Components/AeTooltip/AeTooltip.razor.cs
+++ AElemental/Components/AeTooltip/AeTooltip.razor.cs
@@ -1,4 +1,5 @@
-﻿namespace AElemental.Components;
+﻿// ReSharper disable once CheckNamespace
+namespace AElemental.Components;
 
 public partial class AeTooltip
 {
@@ -15,12 +16,5 @@ public partial class AeTooltip
         public const string Start = "start";
         public const string End = "end";
         public const string Center = "center";
-    }
-
-    public struct States
-    {
-        // There should be four states open, closed, siyad, and undecided
-        public const string Open = "open";
-        
     }
 }
