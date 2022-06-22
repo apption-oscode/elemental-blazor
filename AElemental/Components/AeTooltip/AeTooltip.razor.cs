6c4940275ed78158a193dd3c1adae86c1704e272
Yannick Robert <43834308+yjmrobert@users.noreply.github.com>
2022-06-22 07:17:33 -0400

    wip on tooltips

---
+++ AElemental/Components/AeTooltip/AeTooltip.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace AElemental.Components;
+
+public partial class AeTooltip
+{
+    public struct Directions
+    {
+        public const string Left = "left";
+        public const string Right = "right";
+        public const string Top = "top";
+        public const string Bottom = "bottom";
+    }
+
+    public struct Alignments
+    {
+        public const string Start = "start";
+        public const string End = "end";
+        public const string Center = "center";
+    }
+
+    public struct States
+    {
+        // There should be four states open, closed, siyad, and undecided
+        public const string o
+    }
+}
